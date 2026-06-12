6c405aa52c972e7b277f999b33446fc79cc3ddb1
k-tanuj <tanujkumawat3008@gmail.com>
2026-06-12 09:34:07 +0530

    added index.html

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
 
 app = Flask(__name__)
 
-model = joblib.load('lr_model.pkl')
+model = joblib.load('rf_model.pkl')
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json()
-        input_data = np.array(data['input']).reshape(1, -1)
-        prediction = model.predict(input_data)
-        return jsonify({'prediction': int(prediction[0])})
-    except Exception as e:
-        return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
+        input_data = np.array(data['input']).reshape(1, -1)
+
+        prediction = model.predict(input_data)
+
+        return jsonify({
+            "prediction": int(prediction[0])
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
     app.run(debug=True)
